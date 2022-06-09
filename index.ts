28e15da9b3cfc08554ff0bcc55fcac8d9483a724
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-06-09 08:05:15 +0700

    update

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import express, { Express } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { makeLog } from './src/utils/utils';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
-import router from './src/routes/routes';
-import { client } from './src/services/db';
+import router from './src/routes/v1/routes';
+import db from './src/database/db';
 
 const app: Express = express();
 const port = process.env.PORT === undefined ? '7002' : process.env.PORT;
@@ -15,16 +16,9 @@ app.use(cors({
   origin: 'http://localhost:7001',
   credentials: true,
 }));
-
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(router);
-
-client.connect()
-  .then(() => makeLog('db-connection', 'success', true))
-  .catch((e: Error) => makeLog('db-connection', e.stack ? e.stack : e, true));
-
+db.init();
 app.listen(port, () => makeLog('server', `Server is running at http://localhost:${port}`));
