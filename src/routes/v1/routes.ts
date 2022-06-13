aed7f9fe6478a14c7653eaa899acae182fc5b072
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-06-13 08:45:17 +0700

    update routes

--- src/routes/v1/routes.ts
+++ src/routes/v1/routes.ts
@@ -1,15 +1,16 @@
 import express, { Router } from 'express';
 import authController from '../../controllers/authController';
 import errorController from '../../controllers/errorController';
-import { verifyToken } from '../../middlewares/middlewares';
+import middlewares from '../../middlewares/middlewares';
 
 const router: Router = express.Router();
+const {verifyToken} = middlewares;
 
 /**
  * Authentication
  */
 router.post('/api/v1/auth/login', authController.createLogin);
-router.post('/api/v1/auth/logout', verifyToken, authController.deleteLogin);
+router.delete('/api/v1/auth/logout', verifyToken, authController.deleteLogin);
 router.get('/api/v1/auth/role', verifyToken, authController.getCurrentRole);
 router.get('/api/v1/auth/session', verifyToken, authController.getSessionValidation);
 
