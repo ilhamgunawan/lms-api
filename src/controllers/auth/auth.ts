6cb119a8ebf7ecc8c017ae7fd44a00416e42c011
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-06-13 08:46:28 +0700

    remove unused

--- src/controllers/auth/auth.ts
+++
@@ -1,6 +0,0 @@
-import loginController from "./login";
-import logoutController from './logout';
-import currentRoleController from "./current-role";
-
-const authController = { loginController, logoutController, currentRoleController };
-export default authController;
--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -1,5 +1,4 @@
 import usersController from "./users/users";
-import authController from "./auth/auth";
 import menusController from "./menus/menus";
 
-export { authController, usersController, menusController };
+export { usersController, menusController };
