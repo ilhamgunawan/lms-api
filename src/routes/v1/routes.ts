aa95ccfc7755285d4f5bf41f37a427b70b8f5f68
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-06-14 07:32:22 +0700

    update user routes

--- src/routes/v1/routes.ts
+++ src/routes/v1/routes.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
 import authController from '../../controllers/authController';
+import userController from '../../controllers/userController';
 import errorController from '../../controllers/errorController';
 import middlewares from '../../middlewares/middlewares';
 
@@ -13,6 +14,16 @@ router.post('/api/v1/auth/login', authController.createLogin);
 router.delete('/api/v1/auth/logout', verifyToken, authController.deleteLogin);
 router.get('/api/v1/auth/role', verifyToken, authController.getCurrentRole);
 router.get('/api/v1/auth/session', verifyToken, authController.getSessionValidation);
+
+/**
+ * Users
+ */
+router.get('/api/v1/users', verifyToken, userController.getAllUser);
+router.get('/api/v1/users/:id', verifyToken, userController.getUserByID);
+// TODO: Add create user controller, add delete user controller, add update user controller
+router.post('/api/v1/users/create', verifyToken, userController.createUser);
+router.patch('/api/v1/users/update/:id');
+router.delete('/api/v1/users/delete/:id');
 
 /**
  * Invalid Route
