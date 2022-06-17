5b3260310b609d7b79df8513c05f8fcb770f2614
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-06-17 07:20:30 +0700

    update routes users

--- src/routes/v1/routes.ts
+++ src/routes/v1/routes.ts
@@ -20,10 +20,9 @@ router.get('/api/v1/auth/session', verifyToken, authController.getSessionValidat
  */
 router.get('/api/v1/users', verifyToken, userController.getAllUser);
 router.get('/api/v1/users/:id', verifyToken, userController.getUserByID);
-// TODO: Add create user controller, add delete user controller, add update user controller
 router.post('/api/v1/users/create', verifyToken, userController.createUser);
-router.patch('/api/v1/users/update/:id');
-router.delete('/api/v1/users/delete/:id');
+router.patch('/api/v1/users/update/:id', verifyToken, userController.updateUser);
+router.delete('/api/v1/users/delete/:id', verifyToken, userController.deleteUser);
 
 /**
  * Invalid Route
