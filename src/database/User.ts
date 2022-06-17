54c40a57937abd95130e650c58ed9ceef0c81fc6
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-06-17 07:19:36 +0700

    add user crud to DB

--- src/database/User.ts
+++ src/database/User.ts
@@ -1,10 +1,21 @@
 import { QueryResult } from 'pg';
 import db from "./db";
 
-interface User {
+export interface User {
   id: string;
   email: string;
   name: string;
+}
+
+export interface CreateUser {
+  email: string;
+  name: string;
+  password: string;
+}
+
+export interface UpdateUser {
+  name: string | null;
+  email: string | null;
 }
 
 function getAllUser(): Promise<QueryResult<User>> {
@@ -21,6 +32,39 @@ function getUserByID(id: string): Promise<QueryResult<User>> {
   );
 }
 
-const User = { getAllUser, getUserByID };
+function createUser(user: CreateUser) {
+  return db.queryPromise(
+    `
+      INSERT INTO users
+      (name, email, password, updated_at, deleted_at)
+      values($1, $2, $3, NULL, NULL)
+    `,
+    [user.name, user.email, user.password],
+  );
+}
+
+function deleteUser(id: string) {
+  return db.queryPromise(
+    `
+      UPDATE users
+      SET deleted_at=now()
+      WHERE id=$1::uuid
+    `,
+    [id],
+  );
+}
+
+function updateUser(set: string, where: string, data: Array<string>) {
+  return db.queryPromise(
+    `
+      UPDATE users
+      SET ${set} updated_at=now()
+      WHERE ${where}
+    `,
+    data,
+  );
+}
+
+const User = { getAllUser, getUserByID, createUser, deleteUser, updateUser };
 
 export default User;
