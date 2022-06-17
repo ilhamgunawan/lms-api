64be3b61fdc03af4a650c0808a693e2d6b111f86
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-06-17 07:19:58 +0700

    update userService

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,5 @@
-import User from "../database/User"
+import User, { UpdateUser } from "../database/User"
+import bcrypt from 'bcrypt';
 
 const getAllUser = async () => {
   const result = await User.getAllUser();
@@ -10,6 +11,39 @@ const getUserByID = async (id: string) => {
   return result.rowCount === 1 ? result.rows[0] : null;
 }
 
-const userService = { getAllUser, getUserByID };
+const createUser = async (name: string, email: string) => {
+  const defaultPassword = process.env.DEFAULT_CREATE_USER_PASS as string;
+  const password = bcrypt.hashSync(defaultPassword, 10);
+  const result = await User.createUser({name, email, password});
+  return result.rowCount;
+}
+
+const deleteUser = async (id: string) => {
+  const result = await User.deleteUser(id);
+  return result.rowCount;
+}
+
+const updateUser = async (id: string, data: UpdateUser) => {
+  let set = "";
+  const where = "id=$1::uuid";
+  const list = [id];
+  if (data.name) {
+    list.push(data.name);
+    const index = list.indexOf(data.name) + 1;
+    set = `name=$${index},`;
+  }
+  if (data.email) {
+    list.push(data.email);
+    const index = list.indexOf(data.email) + 1;
+    set = `${set} email=$${index},`;
+  }
+  if (set) {
+    await User.updateUser(set, where, list);
+    return true;
+  }
+  return false;
+}
+
+const userService = { getAllUser, getUserByID, createUser, deleteUser, updateUser };
 
 export default userService;
