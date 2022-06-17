14d275f1dae2e28d989d3a64885c351f360cf62a
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-06-17 07:20:14 +0700

    update userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -49,14 +49,118 @@ const getUserByID = async (req: Request, res: Response) => {
 };
 
 const createUser = async (req: Request, res: Response) => {
-  const body = req.body;
-  return res.status(200).send({
-    status: 'Ok',
-    data: { user: body },
-    message: 'Success',
-  });
+  try {
+    const name: string = req.body['name'];
+    const email: string = req.body['email'];
+    if (name && email) {
+      const result = await userService.createUser(name, email);
+      if (result === 1) {
+        return res.status(200).send({
+          status: 'Ok',
+          data: null,
+          message: 'Success',
+        });
+      }
+      return res.status(400).send({
+        status: 'Bad request',
+        data: null,
+        message: "You must provide valid name and email",
+      });
+    }
+    return res.status(400).send({
+      status: 'Bad request',
+      data: null,
+      message: "You must provide valid name and email",
+    });
+  } catch(e) {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const err = e as any;
+    console.log({
+      status: 'error',
+      url,
+      error: err.stack,
+    });
+    return res.status(400).send({
+      status: 'Bad request',
+      data: null,
+      message: err.detail,
+    });
+  }
 }
 
-const userController = { getAllUser, getUserByID, createUser };
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params['id'];
+    if (id) {
+      const result = await userService.deleteUser(id);
+      if (result === 1) {
+        return res.status(200).send({
+          status: 'Ok',
+          data: null,
+          message: 'Success',
+        });
+      }
+      return res.status(400).send({
+        status: 'Bad request',
+        data: null,
+        message:  "You must provide a valid user ID",
+      });
+    }
+    return res.status(400).send({
+      status: 'Bad request',
+      data: null,
+      message: "You must provide a valid user ID",
+    });
+  } catch(e) {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const err = e as any;
+    console.log({
+      status: 'error',
+      url,
+      error: err.stack,
+    });
+    return res.status(400).send({
+      status: 'Bad request',
+      data: null,
+      message: "You must provide a valid user ID",
+    });
+  }
+}
+
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params['id'];
+    const name: string | null = req.body['name'] ? req.body['name'] : null;
+    const email: string | null = req.body['email'] ? req.body['email'] : null;
+    const result = await userService.updateUser(id, {name, email});
+    if (result) {
+      return res.status(200).send({
+        status: 'Ok',
+        data: null,
+        message: 'Success',
+      });
+    }
+    return res.status(400).send({
+      status: 'Bad request',
+      data: null,
+      message: "You must provide a valid user's data",
+    });
+  } catch(e) {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const err = e as any;
+    console.log({
+      status: 'error',
+      url,
+      error: err.stack,
+    });
+    return res.status(400).send({
+      status: 'Bad request',
+      data: null,
+      message: "You must provide a valid user ID",
+    });
+  }
+}
+
+const userController = { getAllUser, getUserByID, createUser, deleteUser, updateUser };
 
 export default userController;
