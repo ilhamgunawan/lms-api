e0126329ae72594a72037b95398054c05726ea51
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-02-08 13:05:29 +0700

    Refactor user controller using class based

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,150 +3,122 @@ import knex from '../connection';
 import UserAccountService from '../services/UserAccount';
 import UserLoginDataService from '../services/UserLoginData';
 import PasswordHashService from '../services/PasswordHash';
-import ErrorReporterService from '../services/ErrorReporter';
+import ErrorHandlerService from '../services/ErrorHandler';
 import { messages, responseStatus, errorName } from '../utils/constant';
 
-async function getUserById(req: Request, res: Response) {
-  const trxProvider = knex.transactionProvider();
-  const trx = await trxProvider();
+export default class UserController {
+  static readonly controller = 'UserController';
 
-  try {
-    const userId = req.params.id;
-    const missingFields = [];
-
-    if (!userId) {
-      missingFields.push('id');
-      const err = new Error();
-      err.name = errorName.missingFields;
-      err.message = JSON.stringify(missingFields);
-      throw err;
-    }
-
-    const result = await UserAccountService.getById(trx, userId);
-
-    if (result.err) throw result.err;
-    if (!result.data) {
-      const err = new Error(messages.notFound);
-      err.name = errorName.notFound;
-      throw err;
-    }
-
-    return res.status(responseStatus.ok).send(result);
-  } catch(e) {
-    let message = messages.general;
-    let status = responseStatus.internalServerError;
-
-    if (e instanceof Error) {
+  static async createUser(req: Request, res: Response) {
+    const trxProvider = knex.transactionProvider();
+    const trx = await trxProvider();
+  
+    try {
+      const body = req.body;
+      const missingFields = [];
+      const result: Record<string, any> = {};
       
-      if (e.name === errorName.notFound) {
-        message = e.message;
-        status = responseStatus.notFound;
-      }
-    }
-
-    return res.status(status).send({ message });
-  }
-}
-
-async function createUser(req: Request, res: Response) {
-  const trxProvider = knex.transactionProvider();
-  const trx = await trxProvider();
-
-  try {
-    const body = req.body;
-    const missingFields = [];
-    const result: Record<string, any> = {};
-    
-    if (!body.first_name) missingFields.push('first_name');
-    if (!body.last_name) missingFields.push('last_name');
-    if (!body.gender) missingFields.push('gender');
-    if (!body.date_of_birth) missingFields.push('date_of_birth');
-    if (!body.user_name) missingFields.push('user_name');
-    
-    if (missingFields.length) {
-      const err = new Error();
-      err.name = errorName.missingFields;
-      err.message = JSON.stringify(missingFields);
-      throw err;
-    };
-
-    const userAccountPostResult = await UserAccountService.post(trx, {
-      first_name: body.first_name,
-      last_name: body.last_name,
-      gender: body.gender,
-      date_of_birth: body.date_of_birth,
-    });
-
-    if (userAccountPostResult.err) {
-      throw userAccountPostResult.err;
-    }
-
-    if (userAccountPostResult.data) {
-      let passwordPlain = body.password;
-
-      if (!body.password) {
-        const birthDate = new Date(userAccountPostResult.data.date_of_birth);
-        const year = birthDate.getFullYear();
-        const month = birthDate.getMonth() + 1;
-        const day = birthDate.getDate();
-        passwordPlain = `${month}${day}${year}`;
-      }
-
-      const userLoginDataResult = await UserLoginDataService.post(trx, {
-        user_id: userAccountPostResult.data.id,
-        user_name: body.user_name,
-        psw_hash: PasswordHashService.createHash(passwordPlain),
-      });
-
-      if (userLoginDataResult.err) throw userLoginDataResult.err;
-
-      await trx.commit();
-
-      result.data = {
-        ...userAccountPostResult.data,
-        user_name: userLoginDataResult.data?.user_name,
-        psw_hash: userLoginDataResult.data?.psw_hash,
+      if (!body.first_name) missingFields.push('first_name');
+      if (!body.last_name) missingFields.push('last_name');
+      if (!body.gender) missingFields.push('gender');
+      if (!body.date_of_birth) missingFields.push('date_of_birth');
+      if (!body.user_name) missingFields.push('user_name');
+      
+      if (missingFields.length) {
+        const err = new Error();
+        err.name = errorName.missingFields;
+        err.message = JSON.stringify(missingFields);
+        throw err;
       };
+  
+      const userAccountPostResult = await UserAccountService.post(trx, {
+        first_name: body.first_name,
+        last_name: body.last_name,
+        gender: body.gender,
+        date_of_birth: body.date_of_birth,
+      });
+  
+      if (userAccountPostResult.err) {
+        throw userAccountPostResult.err;
+      }
+  
+      if (userAccountPostResult.data) {
+        let passwordPlain = body.password;
+  
+        if (!body.password) {
+          const birthDate = new Date(userAccountPostResult.data.date_of_birth);
+          const year = birthDate.getFullYear();
+          const month = birthDate.getMonth() + 1;
+          const day = birthDate.getDate();
+          passwordPlain = `${month}${day}${year}`;
+        }
+  
+        const userLoginDataResult = await UserLoginDataService.post(trx, {
+          user_id: userAccountPostResult.data.id,
+          user_name: body.user_name,
+          psw_hash: PasswordHashService.createHash(passwordPlain),
+        });
+  
+        if (userLoginDataResult.err) throw userLoginDataResult.err;
+  
+        await trx.commit();
+  
+        result.data = {
+          ...userAccountPostResult.data,
+          user_name: userLoginDataResult.data?.user_name,
+          psw_hash: userLoginDataResult.data?.psw_hash,
+        };
+      }
+      
+      return res.status(responseStatus.ok).send(result);
+  
+    } catch(e) {
+      const errorHandler = new ErrorHandlerService();
+      return await errorHandler.controllerHandler({
+        req,
+        res,
+        trx,
+        e,
+        controller: `${this.controller}.createUser`,
+      });
     }
-    
-    return res.status(responseStatus.ok).send(result);
+  }
 
-  } catch(e) {
-    let message = messages.general;
-    let status = responseStatus.internalServerError;
-
-    if (e instanceof Error) {
-      message = e.message;
-
-      if (e.name === errorName.missingFields) {
-        message = messages.missingFields(JSON.parse(e.message).join(', '));
-        status = responseStatus.badRequest;
+  static async getUserById(req: Request, res: Response) {
+    const trxProvider = knex.transactionProvider();
+    const trx = await trxProvider();
+  
+    try {
+      const userId = req.params.id;
+      const missingFields = [];
+  
+      if (!userId) {
+        missingFields.push('id');
+        const err = new Error();
+        err.name = errorName.missingFields;
+        err.message = JSON.stringify(missingFields);
+        throw err;
       }
-
-      if (e.name === errorName.badTimestampFormat) {
-        status = responseStatus.badRequest;
+  
+      const result = await UserAccountService.getById(trx, userId);
+  
+      if (result.err) throw result.err;
+      if (!result.data) {
+        const err = new Error(messages.notFound);
+        err.name = errorName.notFound;
+        throw err;
       }
-
-      if (e.name === errorName.duplicateUsername) {
-        status = responseStatus.badRequest;
-        message = messages.duplicateUsername(req.body.user_name);
-      }
+  
+      return res.status(responseStatus.ok).send(result);
+    } catch(e) {
+      const errorHandler = new ErrorHandlerService();
+      return await errorHandler.controllerHandler({
+        req,
+        res,
+        trx,
+        e,
+        controller: `${this.controller}.getUserById`,
+      });
     }
-
-    ErrorReporterService.controllerError({
-      controller: 'createUser',
-      message,
-    });
-
-    await trx.rollback();
-
-    return res.status(status).send({ message });
   }
 }
-
-const userController = { 
-  createUser, 
-  getUserById, 
-};
-
-export default userController;
--- src/routes/v1/routes.ts
+++ src/routes/v1/routes.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 import authController from '../../controllers/auth';
-import userController from '../../controllers/user';
+import UserController from '../../controllers/User';
 import errorController from '../../controllers/errorController';
 import middlewares from '../../middlewares/middlewares';
 import authMiddlewares from '../../middlewares/auth';
@@ -26,8 +26,8 @@ router.post('/api/v1/auth/login', authController.login);
 // router.post('/api/v1/users/create', verifyToken, userController.createUser);
 // router.patch('/api/v1/users/:id/update', verifyToken, userController.updateUser);
 // router.delete('/api/v1/users/:id/delete', verifyToken, userController.deleteUser);
-router.get('/api/v1/users/:id', verifyCredentials, userController.getUserById);
-router.post('/api/v1/users/create', verifyCredentials, userController.createUser);
+router.get('/api/v1/users/:id', verifyCredentials, UserController.getUserById);
+router.post('/api/v1/users/create', verifyCredentials, UserController.createUser);
 
 /**
  * Invalid Route
